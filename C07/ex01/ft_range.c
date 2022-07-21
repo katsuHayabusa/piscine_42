3b5ed42b4b7b2846f849065fd690b7efdd4d946b
Samy AICHAOUI <saichaou@e2r11p14.clusters.42paris.fr>
2022-07-21 16:33:43 +0200

    allez

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,13 +6,41 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 14:27:52 by saichaou          #+#    #+#             */
-/*   Updated: 2022/07/21 16:06:09 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/07/21 16:32:02 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stlib.h>
+#include <stdlib.h>
+
+int	ft_len_distance(int a, int b)
+{
+	int	i;
+
+	i = 0;
+	while (a < b)
+	{
+		i++;
+		a++;
+	}
+	return (i);
+}
 
 int	*ft_range(int min, int max)
 {
+	int	*ptr;
+	int	i;
+
+	if (min >= max)
+		return ('\0');
+	ptr = malloc(sizeof(int) * ft_len_distance(min, max));
+	i = 0;
+	while (min < max)
+	{
+		ptr[i] = min;
+		i++;
+		min++;
+	}
+	return (ptr);
+
 
 }
