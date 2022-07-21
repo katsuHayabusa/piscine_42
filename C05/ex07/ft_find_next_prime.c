3fa9799f370f8206733a5d5d270d37e80811dd70
Samy AICHAOUI <saichaou@e2r11p14.clusters.42paris.fr>
2022-07-21 12:25:08 +0200

    we

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,11 +6,31 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 14:24:17 by saichaou          #+#    #+#             */
-/*   Updated: 2022/07/20 15:50:06 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/07/21 10:37:06 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int     ft_is_prime(int nb)
+{
+        int     i;
+
+        if (nb <= 1)
+                return (0);
+        i = 2;
+        while (i < nb)
+        {
+                if (nb % i == 0)
+                        return (0);
+                i++;
+        }
+        return (1);
+}
+
 int	ft_find_next_prime(int nb)
 {
-	
+	if (nb <= 2)
+		return (2);
+	while (!ft_is_prime(nb))
+		return (ft_find_next_prime(nb + 1));
+	return (nb);
 }
---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/21 11:53:25 by saichaou          #+#    #+#             */
+/*   Updated: 2022/07/21 12:18:28 by saichaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int     ft_strcmp(char *s1, char *s2)
+{
+        int     i;
+
+        i = 0;
+        while (s1[i] == s2[i] && s1[i])
+                i++;
+        return (s1[i] - s2[i]);
+}
+
+void	**sort_params(char **params)
+{
+	int	i;
+	int	j;
+	char	*temp;
+
+	i = 1;
+	j = 1;
+	while (params[i])
+	{
+		j = i + 1;
+		while (params[j])
+		{
+			if (ft_strcmp(params[i],params[j]) > 0)
+			{
+				temp = params[i];
+				params[i] = params[j];
+				params[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (!argv[i])
+		return ('\0');
+	sort_params(argv);
+	while (argv[i])
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
