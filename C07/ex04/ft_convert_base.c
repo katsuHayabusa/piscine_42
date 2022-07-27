dee9cd0f6d8e8bef6145ca9cf218c7364130f574
Samy AICHAOUI <saichaou@e2r10p15.clusters.42paris.fr>
2022-07-27 20:14:21 +0200

    nique ta mere ronen

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,25 +6,20 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 15:57:07 by saichaou          #+#    #+#             */
-/*   Updated: 2022/07/27 13:20:35 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/07/27 18:44:36 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     ft_iterative_power(int nb, int power)
+#include <stdio.h>
+
+int     str_len(char *str)
 {
-        int     res;
         int     i;
 
-        i = 1;
-        res = 1;
-        if (power < 0)
-                return (0);
-        while (i <= power)
-        {
-                res *= nb;
+        i = 0;
+        while (str[i])
                 i++;
-        }
-        return (res);
+        return (i);
 }
 
 int     is_valid_base(char *base)
@@ -33,7 +28,7 @@ int     is_valid_base(char *base)
         int     j;
 
         i = 0;
-        if (size(base) < 2 || base[0] <= 47)
+        if (str_len(base) < 2 || base[0] <= 47)
                 return (0);
         while (base[i])
         {
@@ -51,36 +46,44 @@ int     is_valid_base(char *base)
         return (1);
 }
 
-int	convert_base10(char *nbr, char *base)
+int	char_index(char *str, char c)
 {
 	int	i;
-	int	pow;
-	int	res;
 
 	i = 0;
-	res = 0;
-	while (base[i])
-		i++;
-	while (base[i])
+	while (str[i])
 	{
-		res += 
+		if (str[i] == c)
+			return (i);
+		i++;
 	}
-
-
-
-
-
+	return (0);
 }
 
-char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+int	ft_atoi_base(char *nbr, char *base_from)
 {
 	int	i;
-	int	j;
+	int	res;
+	int	sign;
 
 	i = 0;
-	if (!is_valid_base(*base_from) || !is_valid_base(*base_to))
-		return (NULL);
+	sign = 1;
+	res = 0;
+	if (!is_valid_base(base_from))
+		return (0);
 	while ((nbr[i] >= 9 && nbr[i] <= 13) || nbr[i] == 32)
 		i++;
-
+	while (nbr[i] == '+' || nbr[i] == '-')
+	{
+		if (nbr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (char_index(base_from, nbr[i]))
+	{
+		res *= str_len(base_from);
+		res += char_index(base_from, nbr[i]);
+		i++;
+	}
+	return (res * sign);
 }
